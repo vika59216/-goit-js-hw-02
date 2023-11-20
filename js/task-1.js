5f01e3b866890c537b60a191a8d7492645a06fa2
vika59216 <viktoriiadupreez@gmail.com>
2023-11-20 20:51:56 +0100

    hw2

--- js/task-1.js
+++ js/task-1.js
@@ -1,13 +1,22 @@
 
 
-function makeTransaction(quantity, pricePerDroid) {
-     const totalPrice = quantity * pricePerDroid;
-    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-    
-    }
-   
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
+     let totalPrice = quantity + pricePerDroid;
+     if (totalPrice > customerCredits) {
+          return `Insufficient funds!`;
+     } else {
+          return `You ordered ${quantity} droids worth ${totalPrice} credits!`
+     }
+}
 
 
-    makeTransaction(5, 3000);
-    makeTransaction(3, 1000);
-    makeTransaction(10, 500);
+
+
+
+
+
+console.log(makeTransaction(5, 3000, 23000));  
+console.log(makeTransaction(3, 1000, 15000)); 
+console.log(makeTransaction(10, 5000, 8000)); 
+console.log(makeTransaction(8, 2000, 10000)); 
+console.log(makeTransaction(10, 500, 5000)); 
--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,21 @@
 
+function checkForSpam(message) {
+    const lowerCaseMessage = message.toLowerCase();
+    if (lowerCaseMessage.includes('spam') || lowerCaseMessage.includes('sale')) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
-function getElementWidth(content, padding, border) {
-    return parseFloat(content) + 2 * (parseFloat(padding) + parseFloat(border));
-     }
 
 
 
-console.log(getElementWidth("50px", "8px", "4px")); 
-console.log(getElementWidth("60px", "12px", "8.5px")); 
-    console.log(getElementWidth("200px", "0px", "0px")); 
 
-   
+console.log(checkForSpam("Latest technology news")); 
+console.log(checkForSpam("JavaScript weekly newsletter")); 
+console.log(checkForSpam("Get best sale offers now!")); 
+console.log(checkForSpam("Amazing SalE, only tonight!")); 
+console.log(checkForSpam("Trust me, this is not a spam message"))
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); 
+console.log(checkForSpam("[SPAM] How to earn fast money?")); 
